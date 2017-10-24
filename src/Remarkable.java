f7f86d7a8e95075f0439b3ec941182962f83dbf4
ShadowWolf <league@WTS15.attlocal.net>
2017-10-23 18:15:29 -0700

    Variables 3, Remarkable(IP), Secret Message Box(Haven't Started)

---
+++ src/Remarkable.java
@@ -0,0 +1,26 @@
+import javax.swing.JOptionPane;
+
+public class Remarkable {
+	public static void main(String[] args) {
+		String name= JOptionPane.showInputDialog("What is Your Name");
+			
+		if(name.equals("Sam")) {
+			JOptionPane.showMessageDialog(null,"You are great");
+			
+			
+		}
+		else if(name.equals("Peter")){
+		JOptionPane.showMessageDialog(null,"You are awesome");
+		}
+		
+		
+		else if(name.equals("Neal")){
+			JOptionPane.showMessageDialog(null, "You are the Best");
+			
+			else if(name.equals("Tate")){
+		}
+			
+	}
+	
+	}
+
