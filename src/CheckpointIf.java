b09ae3b4c795294c2257ac8a3b9ad0df2278a8ba
nealparth <league@WTS-iMac-11.attlocal.net>
2017-11-06 18:23:29 -0800

    CheckpointIf

---
+++ src/CheckpointIf.java
@@ -0,0 +1,63 @@
+import javax.swing.JOptionPane;
+
+public class CheckpointIf {
+	public static void main(String[] args) {
+		String message="yes";
+		if(message.equals("yes")) {
+		System.out.println("happy");
+		}
+		else {
+			System.out.println("sad");
+		}
+		System.out.println("wednesday");
+	
+		
+		
+		int i = 20;
+		
+		if(i<20) {
+			System.out.println("It's so great being young");
+		}
+		else {
+			System.out.println("Don't you wish you were young");
+			
+			String a = "dog";
+			 
+			JOptionPane.showInputDialog("What is your favorite animal");
+			if(a.equals("dog")) {
+			System.out.println("Dogs go woof");
+			}
+			else {
+				System.out.println("I dont know this animal");
+				
+			}
+			}
+			String d= "cat";
+			if(d.equals("cat")) {
+				System.out.println("Cats go meow");
+			}
+				else {
+					System.out.println("I dont know this animal");
+					
+			
+			
+			
+			
+		}
+		
+		
+				
+				
+				
+				
+				
+				
+				
+				
+	}
+}
+
+
+
+
+
