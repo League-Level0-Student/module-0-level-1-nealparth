6e8199ae0a78760a5ba55f43dd3a01bcc92153ce
nealparth <league@WTS-iMac-11.attlocal.net>
2017-11-13 18:27:41 -0800

    Badgers

---
+++ src/Badgers.java
@@ -0,0 +1,54 @@
+
+public class Badgers {
+	public static void main(String[] args) {
+		String animal= "Badger";
+		String plant= "mushroom";
+		for(int s= 0; s<3; s++) {
+			for(int i= 0; i<10; i++) {
+				System.out.print("Badger, ");
+			}
+				System.out.println(" ");
+				for(int k= 0; k<2; k++) {
+					System.out.print("mushroom, ");
+			}
+				System.out.println("a ");
+		}
+		
+		
+		
+				
+								
+/*				for(int s= 0; s<2; s++) {
+					System.out.print("Mush-mushroom, ");
+				}
+					
+					
+					for(int d= 0; d<1; d++) {
+							System.out.print("African! Snake, a snake, ");
+						}
+					System.out.println(" ");
+					for(int g= 0; g<1; g++) {
+						System.out.print("Snake! A snake, ooh it's a snake ");
+						
+						for(int h= 0; h<1; h++) {
+							System.out.print("It's a badger, badger, badger, badger, badger, badger, badger, badger, badger, badger ");
+						}
+						
+						for(int s= 0; s<2; s++) {
+							System.out.print("mushroom, ");
+							System.out.println(" a ");
+						}*/
+					
+				}
+	}
+
+
+			
+		
+	
+
+
+	
+	
+	
+
